f111e34c42f7dfa5e4912bb7b557072706f4923f
AlanJager <873863981@qq.com>
2017-02-16 11:00:45 +0800

    filter shareable volume in results add UT

---
+++ test/src/test/java/org/zstack/test/compute/vm/TestGetVmAttachableDataVolume.java
@@ -0,0 +1,83 @@
+package org.zstack.test.compute.vm;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.zstack.core.cloudbus.CloudBus;
+import org.zstack.core.componentloader.ComponentLoader;
+import org.zstack.core.db.DatabaseFacade;
+import org.zstack.core.db.SimpleQuery;
+import org.zstack.header.configuration.DiskOfferingVO;
+import org.zstack.header.configuration.DiskOfferingVO_;
+import org.zstack.header.vm.APIGetVmAttachableDataVolumeMsg;
+import org.zstack.header.vm.APIGetVmAttachableDataVolumeReply;
+import org.zstack.header.vm.VmInstanceInventory;
+import org.zstack.header.volume.VolumeInventory;
+import org.zstack.header.volume.VolumeStatus;
+import org.zstack.header.volume.VolumeVO;
+import org.zstack.test.Api;
+import org.zstack.test.ApiSenderException;
+import org.zstack.test.DBUtil;
+import org.zstack.test.deployer.Deployer;
+
+/**
+ * Created by zouye on 2017/2/18.
+ */
+public class TestGetVmAttachableDataVolume {
+    Deployer deployer;
+    Api api;
+    ComponentLoader loader;
+    CloudBus bus;
+    DatabaseFacade dbf;
+
+    @Before
+    public void setUp() throws Exception {
+        DBUtil.reDeployDB();
+        deployer = new Deployer("deployerXml/vm/TestAttachVolumeToVm.xml");
+        deployer.build();
+        api = deployer.getApi();
+        loader = deployer.getComponentLoader();
+        bus = loader.getComponent(CloudBus.class);
+        dbf = loader.getComponent(DatabaseFacade.class);
+    }
+
+    @Test
+    public void test() throws ApiSenderException, InterruptedException {
+        SimpleQuery<DiskOfferingVO> dq = dbf.createQuery(DiskOfferingVO.class);
+        dq.add(DiskOfferingVO_.name, SimpleQuery.Op.EQ, "TestDataDiskOffering");
+        DiskOfferingVO dvo = dq.find();
+        VolumeInventory vinv = api.createDataVolume("TestData", dvo.getUuid());
+
+        vinv.setShareable(true);
+
+        VmInstanceInventory vminv = api.listVmInstances(null).get(0);
+        vinv = api.attachVolumeToVm(vminv.getUuid(), vinv.getUuid());
+
+        Assert.assertTrue(vinv.isAttached());
+
+        Assert.assertEquals(VolumeStatus.Ready.toString(), vinv.getStatus());
+
+        Assert.assertNotNull(vinv.getPrimaryStorageUuid());
+
+        Assert.assertNotNull(vinv.getVmInstanceUuid());
+
+        VolumeVO vvo = dbf.findByUuid(vinv.getUuid(), VolumeVO.class);
+
+        Assert.assertTrue(vvo.isAttached());
+
+        Assert.assertEquals(VolumeStatus.Ready, vvo.getStatus());
+
+        Assert.assertNotNull(vvo.getPrimaryStorageUuid());
+
+        Assert.assertNull(vvo.getVmInstanceUuid());
+
+        APIGetVmAttachableDataVolumeMsg msg = new APIGetVmAttachableDataVolumeMsg();
+        msg.setVmInstanceUuid(vminv.getUuid());
+
+        APIGetVmAttachableDataVolumeReply reply;
+        reply = api.sendApiMessage(msg, APIGetVmAttachableDataVolumeReply.class);
+
+        Assert.assertTrue(reply.getInventories().isEmpty());
+
+    }
+}
